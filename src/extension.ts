1ef6152fb49fdc2349f6f2c14dc8c2c4c4cc79d8
invobzvr <obzvr@outlook.com>
2022-03-03 19:51:08 +0800

    fix: remove redundant statement

--- src/extension.ts
+++ src/extension.ts
@@ -3,7 +3,6 @@ import { ScriptManager } from './scriptManager';
 
 export function activate(context: vscode.ExtensionContext) {
 	const mngr = new ScriptManager;
-	mngr.initialize();
 
 	context.subscriptions.push(vscode.commands.registerCommand('custom-extensionscript.set', () => mngr.set()));
 	context.subscriptions.push(vscode.commands.registerCommand('custom-extensionscript.run', () => mngr.run()));
