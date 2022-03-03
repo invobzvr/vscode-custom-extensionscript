c1a366463576f94825be8ea100624be39f65401a
invobzvr <obzvr@outlook.com>
2022-03-03 22:33:26 +0800

    docs: update examples

--- examples/formatUntitledPython.js
+++ examples/formatUntitledPython.js
@@ -2,14 +2,15 @@
 const child_process = require('child_process'),
     editor = vscode.window.activeTextEditor,
     document = editor.document,
+    text = document.getText(),
     process = child_process.spawn('autopep8', ['--ignore=E401,E402,E501', '-']);
 process.stdout.on('data', data => {
-    editor.edit(builder => {
+    (data = data.toString()) !== text.replace(/\r/g, '') && editor.edit(builder => {
         const lastLine = document.lineAt(document.lineCount - 1),
             start = new vscode.Position(0, 0),
             end = new vscode.Position(document.lineCount - 1, lastLine.text.length);
-        builder.replace(new vscode.Range(start, end), data.toString());
+        builder.replace(new vscode.Range(start, end), data);
         editor.selection = new vscode.Selection(start, start);
     });
 });
-process.stdin.end(document.getText());
+process.stdin.end(text);
